b2f92dea7d577c79687f01081472bcdbdca9a37d
Ephemeral <ephemeral.those316@slmails.com>
2026-04-02 10:12:19 +0600

    refactor: move getCrop to lib/image.ts

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,7 @@ import Footer from "./components/Footer"
 import { createNodes, fetchAnilist } from "./lib/anilist"
 import type { AnilistData } from "./types/anilist"
 import { generateHexagonRing } from "./lib/grid"
-
-function getCrop(
-  boxWidth: number,
-  boxHeight: number,
-  imgWidth: number,
-  imgHeight: number
-) {
-  const boxRatio = boxWidth / boxHeight
-  const imgRatio = imgWidth / imgHeight
-
-  if (boxRatio > imgRatio) {
-    const w = imgWidth
-    const h = imgWidth / boxRatio
-    const x = 0
-    const y = (imgHeight - h) / 2
-    return { x, y, w, h }
-  } else {
-    const w = imgHeight * boxRatio
-    const h = imgHeight
-    const x = (imgWidth - w) / 2
-    const y = 0
-    return { x, y, w, h }
-  }
-}
+import { getCrop } from "./lib/image"
 
 interface AnilistCache {
   timestamp: number
